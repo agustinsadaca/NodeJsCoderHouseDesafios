a7473550088acd20f0d6133841056b4ed6f99476
agustinsadaca <agustinsadaca@gmail.com>
2021-08-21 22:28:16 -0300

    clase 5

---
+++ Desafio 5 - HandleBars/EJS/servidor.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const { Contenedor } = require("./agustinsadaca");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+	
+	app.set('views', './views')
+	app.set('view engine', 'ejs')
+	
+	app.get('/productos',async (req, res) => {
+		const contenedor = new Contenedor();
+		const productos = await contenedor.getAll()
+		res.render('pages/productos', {
+			productos
+			
+		})
+	})
+	
+	app.post('/productos',async (req, res) => {
+		// url http://localhost:8080/
+		const  {title,thumbnail,price}  = req.body
+		
+		const file = new Contenedor()
+		
+		const guardar = await file.save({
+			title: title ,
+			price: price,
+			thumbnail: thumbnail,
+		})
+		const mensaje = 'correcto'
+		res.redirect('/')
+
+	})
+	
+	app.get('/', (req, res) => {
+		res.render('pages/formulario')
+	})
+
+
+app.listen(8080, () => console.log('Server started on 8080'))
+
+
+
---
+++ Desafio 5 - HandleBars/PUG/servidor.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const { Contenedor } = require("./agustinsadaca");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+	
+	app.set('views', './views')
+	app.set('view engine', 'pug')
+	
+	app.get('/productos',async (req, res) => {
+		const contenedor = new Contenedor();
+		const productos = await contenedor.getAll()
+		res.render('index', {
+			productos
+			
+		})
+	})
+	
+	app.post('/productos',async (req, res) => {
+		// url http://localhost:8080/
+		const  {title,thumbnail,price}  = req.body
+		
+		const file = new Contenedor()
+		
+		const guardar = await file.save({
+			title: title ,
+			price: price,
+			thumbnail: thumbnail,
+		})
+		const mensaje = 'correcto'
+		res.redirect('/')
+
+	})
+
+	app.get('/create', (req, res) => {
+		res.render('formulario')
+	})
+	
+	app.get('/', (req, res) => {
+		res.render('pages/formulario')
+	})
+
+
+app.listen(8080, () => console.log('Server started on 8080'))
+
+
+
