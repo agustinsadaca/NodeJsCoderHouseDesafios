ccca0bfd05f1783c35fb08dc2b127456785adf84
agustinsadaca <agustinsadaca@gmail.com>
2022-04-16 14:57:46 -0300

    camb

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,32 @@ import  Producto from "./services/producto.js";
 import  Carrito from "./services/carrito.js";
 import session from 'express-session'
 import passport from "passport"
+import emoji from 'node-emoji'
 import UserRouter from './routers/userRoutes.js'
+import MessageRouter from './controllers/message.controller.js'
 // import routerProducto from './routers/productos.route.js'
 // import routerCarrito from './routers/carrito.route.js'
 import cookieParser from 'cookie-parser'
 import "./strategies/JwtStrategy.js"
 import  "./strategies/LocalStrategy.js"
 import  "./middlewares/authenticate.js"
-const app = express();
+import handlebars from 'express-handlebars'
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 const corsOptions = {
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200 
 }
 app.use(cors(corsOptions))
 
@@ -37,17 +48,39 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/user', UserRouter)
+
 // app.use('/api/productos', routerProducto) 
 // app.use('/api/carrito', routerCarrito) 
 
-const PORT = process.env.PORT;
+app.engine(
+	'hbs',
+	handlebars({
+		extname: '.hbs',
+		defaultLayout: 'main.hbs',
+		layoutsDir: __dirname + '/views',
+	}),
+)
+app.use(express.static('public'))
+app.set('views', './backend/views')
+app.set('view engine', 'hbs')
+app.get('/', (req, res) => {
+  res.render('main', {
+    layout: 'main'
+  })
+})
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors:{
+		oring:'http://localhost:3000',
+	},
+});
+const Message = new MessageRouter(io)
 
-const server = app.listen(PORT, () => {
+const PORT = process.env.PORT;
+const server = httpServer.listen(PORT, () => {
   console.log(`Servidor express corriendo en port ${PORT}`);
 });
 
+
 server.on("error", (error) => console.log(error));
 
-// {"email":"mail",
-// "firstName":"first",
-// "lastName":"last"}
